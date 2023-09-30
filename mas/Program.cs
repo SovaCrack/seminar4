aad3cc95ff5f7d00d454a7e2d3a2cd36aa355d69
Elena <knif-fly@mail.ru>
2023-09-30 21:07:45 +0300

    Добавила задачу с массивом

---
+++ mas/Program.cs
@@ -0,0 +1,13 @@
+﻿void Mass(int col)
+{
+    int[] arry = new int[col];
+
+    for (int i = 0; i < arry.Length; i++)
+    {
+        arry[i] = new Random().Next(0,10);
+        Console.Write($"{arry[i]}");
+    }
+}
+Console.Write("Ввести длину массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+Mass(n);
