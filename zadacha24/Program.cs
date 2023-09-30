a50b1bb26fe9f71b568707866df1e466aa9e0403
Elena <knif-fly@mail.ru>
2023-09-30 21:09:16 +0300

    Добавила задачу 24

---
+++ zadacha24/Program.cs
@@ -0,0 +1,22 @@
+﻿// Демонстрация решения Напишите программу, которая
+//принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
+
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine($"Введите число А: ");
+int a = int.Parse(Console.ReadLine());
+
+int Sum(int Limit)
+{
+    int sum = 0;
+    for (int i = 1; i <= Limit; i++)
+    {
+        sum +=i;
+    }
+    return sum;
+}
+
+Console.WriteLine($"Сумма чисел от 1 до {a} равна {Sum(a)}");
